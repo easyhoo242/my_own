90fdb20a7cb020708eb9d9524f6959d6e8f416a5
Du <easyhoo2426@163.com>
2021-11-18 00:02:23 +0800

    feat(axios): 添加全局api

---
+++ api/cNodeApi.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+axios.create({
+  baseURL: 'https://cnodejs.org/api/v1',
+  timeout: 30000,
+  withCredentials: true,
+})
+
+export function getTopics () {
+  return axios({
+    method: "get",
+    url: "topics"
+  })
+}
---
+++ api/index.js
@@ -0,0 +1,9 @@
+import axios from "axios"
+
+let axiosInstance = axios.create({
+  baseURL: 'https://cnodejs.org/api/v1',
+  timeout: 30000,
+  withCredentials: true,
+})
+
+export default axiosInstance
