848963ebf52b6cffa5f54028363d50114b3b85e0
Du Cheng <ducheng@itjuzi.com>
2021-11-18 17:24:46 +0800

    feat(module): export&import

---
+++ _#dc/module/module1/a.js
@@ -0,0 +1,18 @@
+export const PI = Math.PI
+
+export function degreesToRadians (d) {
+  return d * PI / 180
+}
+
+export class Circle {
+  constructor(r) {
+    this.r = r
+  }
+  area () {
+    return PI * this.r * this.r
+  }
+}
+
+
+
+export { PI, degreesToRadians, Circle }
