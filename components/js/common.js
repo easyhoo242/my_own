71b26d0f6a2408b5985a49a47e3b8d75bd5eefbb
Du Cheng <ducheng@itjuzi.com>
2021-11-23 18:33:07 +0800

    feat(components): 调整commonjs的位置

--- components/js/common.js
+++ components/js/common.js
@@ -2,6 +2,12 @@ const map = (a, ...args) => a.map(...args)
 const reduce = (a, ...args) => a.reduce(...args)
 const sum = (x, y) => x + y
 
+const _splitLine = ( res => {
+  return logR = res => {
+    console.log(new Array(30).join(res))
+  }
+  res ? logR(res) : logR('-')
+})()
 
 
 
