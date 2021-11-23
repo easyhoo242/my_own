268755b76e5af7487af2be28f850cd789707d2da
DuCheng <easyhoo2426@163.com>
2021-11-23 20:28:16 +0800

    faet(api): 暂时注释掉api

--- api/cNodeApi.js
+++ api/cNodeApi.js
@@ -1,14 +1,14 @@
-import axios from 'axios'
+// import axios from 'axios'
 
-axios.create({
-  baseURL: 'https://cnodejs.org/api/v1',
-  timeout: 30000,
-  withCredentials: true,
-})
+// axios.create({
+//   baseURL: 'https://cnodejs.org/api/v1',
+//   timeout: 30000,
+//   withCredentials: true,
+// })
 
-export function getTopics () {
-  return axios({
-    method: "get",
-    url: "topics"
-  })
-}
+// export function getTopics () {
+//   return axios({
+//     method: "get",
+//     url: "topics"
+//   })
+// }
--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
-import axios from "axios"
+// import axios from "axios"
 
-let axiosInstance = axios.create({
-  baseURL: 'https://cnodejs.org/api/v1',
-  timeout: 30000,
-  withCredentials: true,
-})
+// let axiosInstance = axios.create({
+//   baseURL: 'https://cnodejs.org/api/v1',
+//   timeout: 30000,
+//   withCredentials: true,
+// })
 
-export default axiosInstance
+// export default axiosInstance
