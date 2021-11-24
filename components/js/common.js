68c7e51e947b99ad6a42a8369be858775d230146
DuCheng <easyhoo2426@163.com>
2021-11-24 16:27:49 +0800

    feat(function): 一些题目

--- components/js/common.js
+++ components/js/common.js
@@ -2,21 +2,18 @@ const map = (a, ...args) => a.map(...args)
 const reduce = (a, ...args) => a.reduce(...args)
 const sum = (x, y) => x + y
 
-const _splitLine = ( res => {
-  return logR = res => {
-    console.log(new Array(30).join(res))
-  }
+const _splitLine = (res = '-') => {
   res ? logR(res) : logR('-')
-})()
+}
 
-const logR = () => console.log('test')
-
-
+const logR = res => console.log(new Array(30).join(res))
 
 
 
 export {
   map,
   reduce,
-  sum
+  sum,
+  _splitLine,
+  logR
 }
