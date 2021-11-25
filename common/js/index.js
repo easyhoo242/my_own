2e0f73d98880116e54493fbba2122eb526d0fdf9
Du Cheng <ducheng@itjuzi.com>
2021-11-25 11:46:49 +0800

    feat(增加module文件夹)

---
+++ common/js/index.js
@@ -0,0 +1,21 @@
+const map = (a, ...args) => a.map(...args)
+const reduce = (a, ...args) => a.reduce(...args)
+const sum = (x, y) => x + y
+const square = (x, y) => x * x
+
+const _splitLine = (res = '-') => {
+  res ? logR(res) : logR('-')
+}
+
+const logR = res => console.log(new Array(30).join(res))
+
+
+
+export {
+  map,
+  reduce,
+  sum,
+  square,
+  _splitLine,
+  logR
+}
