ded9c23fbadee8b25a084612e34729770fc3202d
Du Cheng <ducheng@itjuzi.com>
2021-12-17 16:43:31 +0800

    feat(ts): pc_general 桔子多维筛选自拟函数练习

--- ts/_pcGeneral/index.ts
+++ ts/_pcGeneral/index.ts
@@ -1,4 +1,3 @@
-
 const ALL_KEY = 'ALL_KEY'
 const __toNormalQuery = (
   data: { key: string; value: string | number }[]
@@ -9,4 +8,14 @@ const __toNormalQuery = (
       [key]: value === ALL_KEY ? undefined : value
     }
   }, {})
+}
+
+
+const toNormalQuery = (data: any) => {
+  return data.reduce((acc: any, { key, value }) => {
+    return {
+      ...acc,
+      [key]: value === ALL_KEY ? undefined : value
+    }
+  })
 }
