60c5d09f8d47cc21f046dd029219fd669ffa1380
Du Cheng <ducheng@itjuzi.com>
2021-12-22 14:37:16 +0800

    feat(tab): 新增tab并绑定点击事件

--- _es6/class/tab栏切换_class/index.js
+++ _es6/class/tab栏切换_class/index.js
@@ -4,7 +4,9 @@ class Tab {
   constructor(id) {
     that = this
     this.tab = document.querySelector(id)
-    console.log(this)
+    this.addBtn = this.tab.querySelector('.btn')
+
+    this.ul = this.tab.querySelector('tab-header ul:first-child')
     this.lis = this.tab.querySelectorAll('li')
     this.secs = this.tab.querySelectorAll('.item')
     this.init()
@@ -12,10 +14,19 @@ class Tab {
 
   // 初始化
   init() {
+    this.upDataList()
+    // 增加选项按钮
+    this.addBtn.onclick = this.addTab
+    // 点击事件初始化
     for (let i = 0; i < this.lis.length; i++) {
       this.lis[i].index = i
       this.lis[i].onclick = this.toggleTab
     }
+  }
+  // 更新list
+  upDataList() {
+    this.lis = this.tab.querySelectorAll('li')
+    this.secs = this.tab.querySelectorAll('.item')
   }
 
   // 清除样式
@@ -27,12 +38,28 @@ class Tab {
   }
   // 切换选项卡
   toggleTab() {
-    that.clearClass() 
+    console.log(1)
+    that.clearClass()
     that.lis[this.index].className = 'active'
     that.secs[this.index].className = 'item active'
   }
+  // 增加选项卡
   addTab() {
+    const newLi = `<li><span>新选项</span></li>`
+    console.log(that)
+    document.querySelector('ul').insertAdjacentHTML('beforeend', newLi)
 
+    const newSec = `
+      <div class="item">
+      新标题<br>
+      新内容
+      </div>
+    `
+    document.querySelector('.tab-content').insertAdjacentHTML('beforeend', newSec)
+    console.log(document.querySelector('ul'))
+    console.log(that, 'addtab')
+
+    that.init()
   }
   deleteTab() {
 
@@ -43,5 +70,3 @@ class Tab {
 }
 
 const tab = new Tab('.tab')
-
-console.log(tab)
