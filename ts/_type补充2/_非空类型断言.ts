9174677839bbb5c9dd75dbd6211c53af51e599e7
DuCheng <easyhoo2426@163.com>
2022-01-01 16:20:17 +0800

    feat(type): 非空类型断言增加注释

--- ts/_type补充2/_非空类型断言.ts
+++ ts/_type补充2/_非空类型断言.ts
@@ -1,10 +1,16 @@
 function printMessageLength(msg?: string) {
+  // 第一种方式是判断类型
   // if (typeof msg === 'string') {
   //   console.log(msg.length)
   // }
+
+  // 第二种方式是非空类型断言
+  // 注意这里的叹号
   console.log(msg!.length)
 }
 
 
 printMessageLength('aaa')
 printMessageLength('hello world')
+
+printMessageLength()
