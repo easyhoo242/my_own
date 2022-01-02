2e02c6ee17df0c6edc129ec8ac0a7118fcda2bf0
Du Cheng <=>
2022-01-03 02:47:29 +0800

    feat(ts): 命名空间

--- ts/_typeScript其他补充内容/src/main.ts
+++ ts/_typeScript其他补充内容/src/main.ts
@@ -1,4 +1,8 @@
-import { sum } from "./math";
+import { sum } from "./utils/math";
+import { time, timer } from './utils/format'
+
+console.log(1)
+console.log(time, timer)
 
 const message: string = "Hello TypeScript";
 console.log(sum(20, 30));
