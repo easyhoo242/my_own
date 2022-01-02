601b59ad912c50add8a8bcf30259128489eaf34e
Du Cheng <=>
2022-01-02 22:34:40 +0800

    feat(interface): 接口的继承

---
+++ ts/_接口/04_接口的继承.ts
@@ -0,0 +1,22 @@
+// 没搞懂
+// 不知道有啥用
+interface IFly {
+  flying: () => void
+}
+
+interface IRunning {
+  running: () => void
+}
+
+interface IWalking extends IFly, IRunning { }
+
+const action: IWalking = {
+  flying() {
+
+  },
+  running() {
+
+  }
+}
+
+export { }
