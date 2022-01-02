00a84efa5f2ab1c0fe533511b146848e0d0002ae
Du Cheng <=>
2022-01-03 03:22:35 +0800

    feat(泛型): 泛型类的使用

---
+++ ts/_泛型/04_泛型类的使用.ts
@@ -0,0 +1,17 @@
+class Point<T> {
+  x: T
+  y: T
+  z: T
+
+  constructor(x: T, y: T, z: T) {
+    this.x = x
+    this.y = y
+    this.z = z
+  }
+}
+
+
+const p1 = new Point('1.2.4', '1.2.4', '1.2.4')
+const p2 = new Point<string>('1.2.4', '1.2.4', '1.2.4')
+// 不推荐
+const p3: Point<string> = new Point('1.2.4', '1.2.4', '1.2.4')
