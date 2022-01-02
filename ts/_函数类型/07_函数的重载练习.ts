b5500e45faf42b7216cddf1aa4a3171c52076c82
Du Cheng <=>
2022-01-02 15:38:39 +0800

    feat(ts): 函数的重载 注释

--- ts/_函数类型/07_函数的重载练习.ts
+++ ts/_函数类型/07_函数的重载练习.ts
@@ -1,5 +1,5 @@
 // 一般情况下 如果联合类型能够实现就使用联合类型
-// 对于这个例子 string和[] 都有length属性 所以使用联合类型更好一些
+// 对于这个例子 string和[] 都有length属性 所以使用联合类型更好一些 
 
 // 联合类型实现
 function getLength(args: string | string[]): number {
