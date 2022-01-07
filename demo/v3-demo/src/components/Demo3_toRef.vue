637d0558dbce9345110f55e0681fc088a79ac890
Du Cheng <=>
2022-01-07 14:28:58 +0800

    (demo): v3 toRefs练习

--- demo/v3-demo/src/components/Demo3_toRef.vue
+++ demo/v3-demo/src/components/Demo3_toRef.vue
@@ -2,16 +2,24 @@
   <div>
     <h2>person</h2>
     <h3>姓名：{{ name }}</h3>
+    <!-- 修改person -->
     <button @click="person.name += '!'">姓名++</button>
     <h3>年龄：{{ age }}</h3>
+    <!-- 修改person -->
     <button @click="person.age++">年龄++</button>
     <h3>工资{{ salary }}</h3>
+    <!-- 修改salary -->
     <button @click="salary++">工资++</button>
+
+    <br />
+    <br />
+    <h4>这里是通过toRefs取出的响应式数据</h4>
+    <h4>{{ `${name} ${age} ${p1.job.salary} ` }}</h4>
   </div>
 </template>
 
 <script>
-import { reactive, toRef } from 'vue'
+import { reactive, toRef, toRefs } from 'vue'
 
 export default {
   name: 'demo3',
@@ -26,11 +34,14 @@ export default {
         }
       }
     })
+
     return {
       person,
       name: toRef(person, 'name'),
       age: toRef(person, 'age'),
-      salary: toRef(person.p1.job, 'salary')
+      salary: toRef(person.p1.job, 'salary'),
+
+      ...toRefs(person)
     }
   }
 }
