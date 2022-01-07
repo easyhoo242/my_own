161e0ea73649eee8127407c6e4ae2dd89eee7ca0
Du Cheng <=>
2022-01-07 11:36:49 +0800

    feat(demo): v3 hooks

--- demo/v3-demo/src/components/Demo2_hooks.vue
+++ demo/v3-demo/src/components/Demo2_hooks.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>{{ num }} <button @click="num++">++</button></div>
+  <div>你点击位置的坐标是: {{ point }}</div>
 </template>
 
 <script>
@@ -8,11 +8,13 @@ import getPoint from '../hooks/getPoint'
 
 export default {
   name: 'demo2',
-  setup() { 
+  setup() {
     const num = ref(0)
-    getPoint()
+    // 使用hooks
+    const point = getPoint()
     return {
-      num
+      num,
+      point
     }
   }
 }
