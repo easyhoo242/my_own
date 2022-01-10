9590a59017a56889705c01bb65ce6384735355a2
Du Cheng <=>
2022-01-10 11:16:21 +0800

    feat(webpack): dev-serve创建

---
+++ _webpack/06_webpack打包-vue-watch/src/main.js
@@ -0,0 +1,18 @@
+// webpack的话这里必须选择以下的版本 否则不会编译
+import { createApp } from 'vue/dist/vue.esm-bundler'
+
+import App from './vue/App.vue'
+import './js/element'
+import './js/other(from_main)'
+
+
+// 这两个需要安装
+// vue@next
+// @vue/compiler-sfc
+
+// webpack配置loader  vue
+// vue-loader-plugin   webpack插件
+const app = createApp(App)
+
+
+app.mount('#app')
---
+++ _webpack/06_webpack打包-vue-watch/webpack.config.js
@@ -0,0 +1,93 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+// const { DefinePlugin } = require('define-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+// 这jb copy插件有毒似的  tmd  烦死我了
+// vue-loader-plugin
+const { VueLoaderPlugin } = require('vue-loader/dist/index')
+
+module.exports = {
+  // 开发者模式
+  // 打包的内容不会进行丑化
+  mode: 'development',
+  //  建立js映射文件 方便调试代码
+  devtool: 'source-map',
+  // 入口
+  entry: './src/main.js',
+  // 出口的位置
+  output: {
+    filename: 'js/bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    // 这里可以写下asset-module-type的generator配置
+    // assetModuleFilename: 'img/[name]_[hash:6][ext]'
+  },
+  plugins: [
+    // 运行打包时 删除原来output的内容
+    new CleanWebpackPlugin(),
+    // 自动生成一个html的文件
+    new HtmlWebpackPlugin({
+      template: './public/index.html',   //可以给一个模版
+      title: '我是标题'
+    }),
+    // new webpack.DefinePlugin({
+
+    // }),
+    new VueLoaderPlugin()
+  ],
+  module: {
+    rules: [
+      // css-loader
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      // asset-module-type  用来取代url-loader & file-loader (webpack5)
+      // webpack5内置  不需要安装loader
+      {
+        test: /\.(jp?eg|png|gif|svg)$/,
+        type: 'asset',
+        generator: {
+          // 不需要单独写 "."
+          filename: 'img/[name]_[hash:6][ext]'
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 288 * 1024,
+          }
+        }
+      },
+      // {
+      //   test: /\.js$/,
+      //   use: {
+      //     loader: 'babel-loader',
+      //     options: {
+      //       // plugins: [
+
+      //       //   // 将es6代码转化成es5
+      //       //   // npm install -D @babel/plugin-transform-arrow-functions
+      //       //   // npm install -D @babel/plugin-transform-block-scoping
+      //       //   '@babel/plugin-transform-arrow-functions',
+      //       //   '@babel/plugin-transform-block-scoping'
+      //       // ]
+      //       plugins: {
+      //         preset: [
+      //           '@babel/preset-env'
+      //         ]
+      //       }
+      //     }
+      //   }
+      // },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  }
+}
