ac68727b7987d38753f520f1612d1b7da123fa10
Du Cheng <=>
2022-01-12 14:52:40 +0800

    feat(v3): 组件的css作用域

---
+++ v3/_components/src/02_组件的css作用域/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    App
+    <h2>test</h2>
+    <Header />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from './header.vue'
+import Footer from './footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer,
+  },
+}
+</script>
+
+<style scoped>
+h2 {
+  color: red;
+}
+</style>
