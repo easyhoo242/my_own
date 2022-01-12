6872337c54dd07ef90b007b8824c3391ee1b4e6d
Du Cheng <=>
2022-01-12 16:43:06 +0800

    feat(v3): 04子组件传参

--- v3/_components/src/04_子组件传递给父组件.vue/App.vue
+++ v3/_components/src/04_子组件传递给父组件.vue/App.vue
@@ -1,13 +1,32 @@
 <template>
   <div class="header">
     <div class="title">头部标题</div>
+    <h1>{{ count }}</h1>
+    <CounterOperation @add="addOne" @sub="subOne" />
   </div>
 </template>
 
 <script>
+import CounterOperation from './counterOperation.vue'
+import { ref } from 'vue'
+
 export default {
+  components: {
+    CounterOperation,
+  },
+  methods: {
+    addOne() {
+      this.count++
+    },
+    subOne() {
+      this.count--
+    },
+  },
   setup() {
-    return {}
+    const count = ref(0)
+    return {
+      count,
+    }
   },
 }
 </script>
---
+++ v3/_components/src/04_子组件传递给父组件.vue/counterOperation.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button @click="decrement">-</button>
+    <button @click="increment">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  emit: ['add', 'sub'],
+  methods: {
+    increment() {
+      console.log('+')
+      this.$emit('add')
+    },
+    decrement() {
+      console.log('-')
+      this.$emit('sub')
+    },
+  },
+}
+</script>
+
+<style scoped></style>
