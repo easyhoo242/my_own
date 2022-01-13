336da9e04258ae68a2b705355201b6b128b3675b
Du Cheng <=>
2022-01-13 09:42:11 +0800

    feat(components): 子传父发消息

--- v3/_components/src/05_综合练习/App.vue
+++ v3/_components/src/05_综合练习/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div class="container">
     <TabContent :tabList="tabList" @getIndex="checkContent" />
+
     {{ contentList[currentIndex] + '模块' }}
+
+    <hr />
+
+    <h3>接收ShowMsg传来的消息：</h3>
+    <h3 class="getMsg">{{ msg }}</h3>
+    <ShowMsg @sendMsg="getMsg" />
   </div>
 </template>
 
 <script>
 import TabContent from './tabContent.vue'
+import ShowMsg from './ShowMsg.vue'
 
 export default {
   components: {
     TabContent,
+    ShowMsg,
   },
   data() {
     return {
       currentIndex: 0,
       tabList: ['电影', '音乐', '户外', '室内'],
       contentList: ['电影', '音乐', '户外', '室内'],
+      msg: '',
     }
   },
   methods: {
     checkContent(index) {
       console.log(index)
       this.currentIndex = index
+    },
+    getMsg(msg) {
+      this.msg = msg
     },
   },
 }
@@ -35,5 +48,14 @@ export default {
   justify-content: center;
   align-items: center;
   margin: 100px auto 0;
+
+  .getMsg {
+    border: 1px solid #ff8200;
+    border-radius: 15px;
+    background-color: #ff8000bd;
+    padding: 5px;
+
+    color: #fff;
+  }
 }
 </style>
---
+++ v3/_components/src/05_综合练习/ShowMsg.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <h2>components-ShowMsg</h2>
+    <input type="text" v-model="msg" />
+    <br />
+    <button @click="postMsg">点击发送msg</button>
+    <br />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: '这是showMsg的默认msg',
+    }
+  },
+  emit: ['sendMsg'],
+  methods: {
+    postMsg() {
+      this.$emit('sendMsg', this.msg)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  width: 500px;
+  border: 2px solid #ff8200;
+
+  input {
+    width: 300px;
+    text-align: center;
+  }
+}
+</style>
