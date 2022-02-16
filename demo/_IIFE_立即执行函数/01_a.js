269dae1da9546f40e977457386773add3aca5e51
Du Cheng <=>
2022-02-16 13:19:56 +0800

    feat(demo): 立即执行函数测试

---
+++ demo/_IIFE_立即执行函数/01_a.js
@@ -0,0 +1,22 @@
+var a = 1
+  ; (function () {
+    var a = 2
+    console.log(this)
+    console.log(a)
+  })()
+
+console.log('---------------')
+
+  ; (function () {
+    console.log(this)
+    console.log(a)
+  })()
+
+
+console.log('---------------')
+
+  ; (function a() {
+    var a = 3
+    console.log(this)
+    console.log(a)
+  })()
