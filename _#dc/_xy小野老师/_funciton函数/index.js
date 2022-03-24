31db6608ddf3af7487cd70cfff2ad42372e71687
Du Cheng <=>
2022-03-24 16:52:52 +0800

    feat(dc): 函数与xy_homework

---
+++ _#dc/_xy小野老师/_funciton函数/index.js
@@ -0,0 +1,46 @@
+
+// var test1 = function test() {
+//   var a = 1
+//   var b = 2
+//   // 函数内部可以调用字面量函数本身 (递归)
+//   // test()
+//   console.log(a, b)
+// }
+
+
+// // console.log(test1.name)   //test
+// test1()
+// // 在外部不能访问到函数字面量函数名
+// test()
+
+
+
+
+// function test(a, b) {
+//   console.log(test.length)  //形参的长度
+//   console.log(arguments.length)  //实参的长度
+// }
+
+// test('x', 'y', 'z')
+
+
+// function test(a, b) {
+//   a = 3
+//   console.log(arguments[0]) // 3
+//   // 函数内部可以修改实参的值
+// }
+
+// test(1)
+
+
+// function test(a, b) {
+//   b = 3
+//   console.log(arguments[1]) // undefined
+//   // 
+// }
+
+// test(1)
+
+
+
+
